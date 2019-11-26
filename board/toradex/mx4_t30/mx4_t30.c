b357adedafc6d854047a7eb618a2ea82beb8813c
rikardo <rikard.olander@hostmobility.com>
2019-11-26 12:04:40 +0100

    Add second eth mac address (t30)

--- board/toradex/mx4_t30/mx4_t30.c
+++ board/toradex/mx4_t30/mx4_t30.c
@@ -57,6 +57,15 @@ int ft_board_setup(void *blob, bd_t *bd)
 		if (err >= 0)
 			puts("   MAC address updated...\n");
 	}
+    /*MAC address (eth1)*/
+	if (eth_getenv_enetaddr("ethaddr2", enetaddr)) {
+		int err = fdt_find_and_setprop(blob,
+				     "/usb@7d008000/asix@2",
+				     "local-mac-address", enetaddr, 6, 0);
+
+		if (err >= 0)
+			puts("   MAC address updated...\n");
+	}
 
 	return ft_common_board_setup(blob, bd);
 }
